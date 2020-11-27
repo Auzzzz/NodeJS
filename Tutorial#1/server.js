94708e1a24c0e316a83af3bab282fdd011b21756
Auzzzz <s3544232@student.rmit.edu.au>
2020-11-28 09:23:22 +1100

    Hello World =P

---
+++ Tutorial#1/server.js
@@ -0,0 +1,14 @@
+// import Express - Web server framework
+const express = require('express');
+const app = express();
+
+// define the port for the webserver to be running on
+const server = app.listen(7000, () => {
+    console.log(`Express running -> PORT ${server.address().port}`);
+})
+
+
+// define a route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
